964adc61a87c9f71a34ecfe4d3543fcdc48b2861
Folarin Obajenihi <folarinobajenihi@gmail.com>
2026-08-01 19:27:17 +0100

    feat: Developer 1 - Navbar, Hero, Footer complete

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/layout/Navbar";
+import Hero from "./components/sections/Hero";
+import Footer from "./components/layout/Footer";
+
+const App = () => {
+  return (
+    <div className="bg-white min-h-screen">
+      <Navbar />
+      <Hero />
+
+      
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
